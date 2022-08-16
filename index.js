6ba98c3eaf05dd02cd5a6cf80dd23d2a1584668e
Anabuki-Kosan <anabuki-cloud@anabuki-kosan.co.jp>
2022-08-16 16:05:17 +0900

    修正

--- index.js
+++ index.js
@@ -85,6 +85,7 @@ server.post("/callback", (req, res) => {
 
 
 server.post("/garoon", (req) => {
+  console.log(req)
   const messageText = req.body;
   const accountId = "ce9a29af-7b61-464e-1de9-04bb22d06597"
   getJWT(jwttoken => {
